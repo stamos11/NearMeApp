037af37508bb9057f20dd47dde4c86c4da7c282b
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-05-18 23:34:45 +0300

    Adding a map Using MapKit

--- NearMeApp/ViewController.swift
+++ NearMeApp/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    //MARK: -Properties
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+//        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    //MARK: -Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupUI()
     }
 
-
+    
+//MARK: -Helpers
+    private func setupUI() {
+        
+        view.addSubview(mapView)
+        
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
 
